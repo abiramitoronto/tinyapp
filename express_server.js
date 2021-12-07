b476e1bacf4f54318b2bf2eae6ff68af46af9e9a
abiramitoronto <nagaabirami.toronto@gmail.com>
2021-12-06 22:35:09 -0500

    Form Submission Using POST method

--- express_server.js
+++ express_server.js
@@ -9,6 +9,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+    return "abcde" + Math.floor(Math.random() * 4);
+}
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -26,9 +32,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok Short URL is " + generateRandomString());         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
